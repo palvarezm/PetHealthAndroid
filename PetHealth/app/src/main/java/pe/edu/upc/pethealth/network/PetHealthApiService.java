2218153f2c53ed8959ed52a4448116f03aaf1913
Paul <palvareze7@gmail.com>
2019-05-29 17:41:32 -0500

    Add pet

--- PetHealth/app/src/main/java/pe/edu/upc/pethealth/network/PetHealthApiService.java
+++ PetHealth/app/src/main/java/pe/edu/upc/pethealth/network/PetHealthApiService.java
@@ -13,7 +13,7 @@ public class PetHealthApiService {
     public static String SIGNUP_CUSTOMER = "http://leodev94-001-site1.dtempurl.com/user/registerCustomer";
     public static String VETERINARY_URL = "http://leodev94-001-site1.dtempurl.com/veterinaries";
     public static String CUSTOMER_URL = "http://leodev94-001-site1.dtempurl.com/customers";
-    public static String ADD_PET_URL = "http://leodev94-001-site1.dtempurl.com/pet/add";
+    public static String ADD_PET_URL = "https://pethealthapi.herokuapp.com/api/customers/{userId}/pets";
     public static String APPOINTMENT_URL = "http://leodev94-001-site1.dtempurl.com/pets/appointments";
     public static String ADD_APPOINTMENT_URL = "http://leodev94-001-site1.dtempurl.com/appointments/add";
     public static String VET_URL = "http://leodev94-001-site1.dtempurl.com/vets";
