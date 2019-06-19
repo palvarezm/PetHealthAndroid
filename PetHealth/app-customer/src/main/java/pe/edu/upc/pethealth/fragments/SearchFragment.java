dd37484b37aea62521e81bd7b5f1533e510e62b0
Paul <palvareze7@gmail.com>
2019-06-19 03:52:42 -0500

    Change search fragment to map

--- PetHealth/app-customer/src/main/java/pe/edu/upc/pethealth/fragments/SearchFragment.java
+++ PetHealth/app-customer/src/main/java/pe/edu/upc/pethealth/fragments/SearchFragment.java
@@ -2,25 +2,27 @@ package pe.edu.upc.pethealth.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import pe.edu.upc.pethealth.R;
 import pe.edu.upc.pethealth.activities.MainActivity;
-import pe.edu.upc.pethealth.adapters.SearchAdapters;
-import pe.edu.upc.pethealth.models.Veterinary;
 import pe.edu.upc.pethealth.network.LoggerCallback;
 import pe.edu.upc.pethealth.network.RestClient;
 import pe.edu.upc.pethealth.network.RestView;
@@ -31,10 +33,9 @@ import retrofit2.Response;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SearchFragment extends Fragment {
-    private RecyclerView searchRecyclerView;
-    private SearchAdapters searchAdapters;
-    private RecyclerView.LayoutManager searchLayoutManager;
+public class SearchFragment extends Fragment implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    private SupportMapFragment mapFragment;
 
     private List<pe.edu.upc.lib.Veterinary> veterinaries;
     private SharedPreferencesManager sharedPreferencesManager;
@@ -49,24 +50,24 @@ public class SearchFragment extends Fragment {
         // Inflate the layout for this fragment
         ((MainActivity)getActivity()).setFragmentToolbar("Search",true,getFragmentManager());
         View view = inflater.inflate(R.layout.fragment_search, container, false);
+        mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.veterinariesMap);
+
         sharedPreferencesManager = SharedPreferencesManager.getInstance(this.getContext());
-        searchRecyclerView = (RecyclerView) view.findViewById(R.id.searchRecyclerView);
         veterinaries = new ArrayList<>();
-        updateSearch(view);
+        updateSearch();
         return view;
     }
 
-    private void updateSearch(View view){
+    private void updateSearch(){
+
+        mapFragment.getMapAsync(this);
         Call<RestView<JsonArray>> call = new RestClient().getWebServices().getCloseVeterinaries(sharedPreferencesManager.getAccessToken(), -12.0874509, -77.0499422);
         call.enqueue(new LoggerCallback<RestView<JsonArray>>(){
             @Override
             public void onResponse(Call<RestView<JsonArray>> call, Response<RestView<JsonArray>> response) {
                 super.onResponse(call, response);
                 parseResponse(response.body().getData());
-                searchAdapters = new SearchAdapters(veterinaries);
-                searchLayoutManager = new LinearLayoutManager(view.getContext());
-                searchRecyclerView.setAdapter(searchAdapters);
-                searchRecyclerView.setLayoutManager(searchLayoutManager);
             }
 
             @Override
@@ -85,5 +86,15 @@ public class SearchFragment extends Fragment {
             Log.d("Veterinary", veterinary.toString());
             veterinaries.add(veterinary);
         }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
